8a76ec03d6c706cbf21a3e75dd684078f3578675
Nesa570 <linzawng00@gmail.com>
2025-12-26 21:33:12 +0630

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,8 +18,6 @@ def load_model():
     except FileNotFoundError:
         st.error("❌ 'Walmart.pkl' or 'Walmart_features.pkl' not found in app folder.")
         return None, None
-        
-rf_model, feature_columns = load_model()
 
 # ---- Load Product Lookup ----
 @st.cache_data
